657b5d0c0612411bfc707c609bd088b667deedfa
Andrei Stelmakh <andrey.stelmakh@live.ru>
2026-02-08 22:19:08 +0000

    UseSession

--- Program.cs
+++ Program.cs
@@ -2,8 +2,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSession(options =>
+{
+    // options.IdleTimeout = TimeSpan.FromMinutes(20);
+    // другие опции при необходимости
+});
 
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -11,6 +18,8 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
 }
 app.UseRouting();
+
+app.UseSession();
 
 app.UseAuthorization();
 
